b212918adbff8b0e6a53105f329cc6e203952cf8
C4I0 <107209603+caio-farias21@users.noreply.github.com>
2023-02-21 15:28:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let profile = document.querySelector('#profile-image');
+let hand = document.querySelector('#hand');
+let shareBtn = document.querySelector('#share-btn');
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
+
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(() => {
+        hand.style.transform = 'rotate(-15deg)';
+        setTimeout(() => {
+            hand.style.transform = 'rotate(15deg)';
+            setTimeout(() => { hand.style.transform = 'rotate(0deg)'; }, 200);
+        }, 200);
+    }, 1500);
+});
+
+document.querySelector('#share-btn').addEventListener('click', function() {
+    navigator.clipboard.writeText("https://portfolio-caiofarias21.vercel.app/");
+});
+
+document.querySelector('#hand').addEventListener('mouseover', function() {
+        hand.style.transform = 'rotate(-15deg)';
+        setTimeout(() => {
+            hand.style.transform = 'rotate(15deg)';
+            setTimeout(() => { hand.style.transform = 'rotate(0deg)'; }, 200);
+        }, 200);
+});
